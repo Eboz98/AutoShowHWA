47ce9f926ebf8de681a5ac9027923ec3a0fa0e49
Eboz98 <41183432+Eboz98@users.noreply.github.com>
2021-06-15 14:27:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AutoShowHWA
+ Hobby Project for QA Academy with Spring (Week 9) (AutoShow)
---
+++ src/main/java/com/qa/cars/AutoShowHwaApplication.java
@@ -0,0 +1,13 @@
+package com.qa.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoShowHwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoShowHwaApplication.class, args);
+	}
+
+}
